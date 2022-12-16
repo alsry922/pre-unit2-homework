749e95f0ce9320af959c44901ea5ea960d03a17c
alsry922 <alsry922@gmail.com>
2022-12-16 14:32:11 +0900

    first commit

---
+++ src/main/java/pre/unit2/PreUnit2HomeworkApplication.java
@@ -0,0 +1,13 @@
+package pre.unit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreUnit2HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreUnit2HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pre/unit2/PreUnit2HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pre.unit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreUnit2HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
