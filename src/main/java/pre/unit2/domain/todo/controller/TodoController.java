9fc32a73e53356e263ca6c9fccf8b396914f90bd
alsry922 <alsry922@gmail.com>
2022-12-16 15:34:35 +0900

    할일 생성하기 post

--- src/main/java/pre/unit2/domain/todo/controller/TodoController.java
+++ src/main/java/pre/unit2/domain/todo/controller/TodoController.java
@@ -1,6 +1,7 @@
 package pre.unit2.domain.todo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class TodoController {
     @GetMapping("/")
     public String helloTodo() {
         return "To-do Application";
+    }
+
+    @PostMapping("/todos")
+    public String postTodo() {
+        return "create todo";
     }
 }
